15cc1f8f40ff41d69e8de713d31e8efe7d700df2
km6238 <km6238@nyu.edu>
2025-04-03 10:41:58 -0400

    CREATE added

---
+++ CREATE.sql
@@ -0,0 +1,66 @@
+CREATE TABLE User(
+	userID INT PRIMARY KEY,
+	userName VARCHAR(16),
+	password VARCHAR(32),
+	firstName VARCHAR(32),
+	lastName VARCHAR(32)
+);
+
+CREATE TABLE Tag(
+	tagID INT PRIMARY KEY,
+	tagName VARCHAR(16)
+);
+
+CREATE TABLE TagList(
+	tagID INT,
+	userID INT,
+	FOREIGN KEY (tagID) REFERENCES Tag(tagID),
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	PRIMARY KEY (tagID, userID)
+);
+
+CREATE TABLE Chat(
+	chatID INT PRIMARY KEY,
+	userID INT,
+	chatName VARCHAR(32),
+	FOREIGN KEY (userID) REFERENCES User(userID)
+);
+
+CREATE TABLE ChatMessage(
+	chatMessageID INT PRIMARY KEY,
+	chatID INT,
+	userID INT,
+	messageText TEXT,
+	TimeStampID INT,
+	FOREIGN KEY (chatID) REFERENCES Chat(chatID),
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (TimeStampID) REFERENCES TimeStamp(TimeStampID)
+);
+
+CREATE TABLE ChatMember(
+	userID INT,
+	chatID INT,
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (chatID) REFERENCES Chat(chatID),
+	PRIMARY KEY (userID, chatID)
+);
+
+CREATE TABLE Comment(
+	commentID INT PRIMARY KEY,
+	userID INT,
+	questionID INT,
+	commentText TEXT,
+	TimeStampID INT,
+	FOREIGN KEY (questionID) REFERENCES Question(questionID),
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (TimeStampID) REFERENCES TimeStamp(TimeStampID)
+);
+
+CREATE TABLE Question(
+	questionID INT PRIMARY KEY,
+	userID INT,
+	questionText TEXT,
+	TimeStampID INT,
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (TimeStampID) REFERENCES TimeStamp(TimeStampID)
+);
