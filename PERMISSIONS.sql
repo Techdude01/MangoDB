117cd78c87653caa038b731b3468fbfb70ef3e6c
km6238 <km6238@nyu.edu>
2025-04-24 13:17:15 -0400

    mangDB

---
+++ PERMISSIONS.sql
@@ -0,0 +1,37 @@
+-- how to run
+-- mysql -u root Mango < PERMISSIONS.sql
+CREATE USER 'mango_user'@'localhost' IDENTIFIED BY 'arfaouiRocks123';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `mango`.* TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`CancelComment` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`CancelCommentDraft` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`CancelQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`CancelResponse` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`CreateChatAndRequest` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`DeleteQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`DeleteUserByUsername` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`DownvoteQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`DraftComment` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`DraftResponse` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetChatIDsForUser` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetChatName` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetControversialQuestionswithPagination` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetPopularQuestionsWithPagination` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetQuestionsByTag` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetQuestionsByUserID` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetRecentQuestionsWithPagination` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetRelevantQuestions` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetSimilarUsersByTags` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetTagsByUserID` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`GetUserCredentials` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`InsertNewUser` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`LogChatAction` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`LogCommentAction` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`LogResponseAction` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`PublishComment` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`PublishQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`PublishResponse` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`RetrieveResponses` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`SearchQuestions` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`StartQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`UpvoteQuestion` TO 'mango_user'@'localhost';
+GRANT EXECUTE ON PROCEDURE `Mango`.`ValidResponse` TO 'mango_user'@'localhost';
